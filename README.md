31ff61eaaf9a0bc264950158dd7d795a88e728f9
yssprojects <76963311+yssprojects@users.noreply.github.com>
2021-01-11 10:04:57 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Image-Server
+
+> A simplest telegram to telegra.ph image uploader bot
+
+## installation (easy way)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/yssprojects/Image-UploadBot/tree/master)
+
+Libraries used: => Pyrogram => Telegraph
+
+Join [@yssprojects](https://t.me/yssprojects)
---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import uuid
+import shutil
+import logging
+from pyrogram import Client, filters
+from creds import Credentials
+from telegraph import upload_file
+
+logging.basicConfig(level=logging.INFO)
+
+
+TGraph = Client(
+    "Image upload bot",
+    bot_token=Credentials.BOT_TOKEN,
+    api_id=Credentials.API_ID,
+    api_hash=Credentials.API_HASH,
+)
+
+
+@TGraph.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text(
+        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by @yssprojects",
+        True,
+    )
+
+
+@TGraph.on_message(filters.photo)
+async def getimage(client, message):
+    tmp = os.path.join("downloads", str(message.chat.id))
+    if not os.path.isdir(tmp):
+        os.makedirs(tmp)
+    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
+    dwn = await message.reply_text("Downloading... \n ©️@yssprojects", True)
+    img_path = await client.download_media(message=message, file_name=img_path)
+    await dwn.edit_text("Uploading...\n **©️**@yssprojects")
+    try:
+        response = upload_file(img_path)
+    except Exception as error:
+        await dwn.edit_text(f"Oops something went wrong\n{error}")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]} \n **©️**@yssprojects")
+    shutil.rmtree(tmp, ignore_errors=True)
+
+
+TGraph.run()
